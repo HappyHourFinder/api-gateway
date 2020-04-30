1fcebae9464e39dc32db956b30484a2a4bc87556
Mathieu Aimé <aime.mathieu1@gmail.com>
2020-04-30 19:47:52 +0200

    WIP Add spring doc

---
+++ src/test/java/com/mathieuaime/hhf/gateway/GatewayApplicationTest.java
@@ -0,0 +1,12 @@
+package com.mathieuaime.hhf.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTest {
+
+  @Test
+  void contextLoad() {
+  }
+}
